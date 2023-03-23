55c6feba9f5a0155ffdf3c7e55e5880de3b1be9a
tudorceltare <tudor.pitic@gmail.com>
2023-03-23 21:14:32 +0100

    Add mock upload image functionality

--- clean-gallen/src/app/citizen-main-app/dashboard/dashboard.component.ts
+++ clean-gallen/src/app/citizen-main-app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import {Component, HostListener, OnInit} from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,41 +7,90 @@ import {Component, HostListener, OnInit} from '@angular/core';
 })
 export class DashboardComponent implements OnInit{
   private fileOver: boolean = true;
+  @ViewChild("fileDropRef", { static: false }) fileDropEl!: ElementRef;
+  files: any[] = [];
+
 
   constructor() {
   }
   ngOnInit(): void {
   }
 
-  @HostListener('dragover', ['$event'])
-  onDragOver(evt) {
-    evt.preventDefault();
-    evt.stopPropagation();
-
-    console.log('Drag Over');
-  }
-  @HostListener('dragleave', ['$event'])
-  onDragLeave(evt) {
-    evt.preventDefault();
-    evt.stopPropagation();
-
-    console.log('Drag Leave');
+  /**
+   * on file drop handler
+   */
+  onFileDropped($event: any) {
+    this.prepareFilesList($event);
   }
 
-  @HostListener('dragdrop', ['$event'])
-  onDragDrop(evt) {
-    evt.preventDefault();
-    evt.stopPropagation();
-    this.fileOver = false;
-    const files = evt.dataTransfer.files;
-    if(files.length > 0) {
-      console.log(`You dropped ${files.length} files here`);
+  /**
+   * handle file from browsing
+   */
+  fileBrowseHandler(event: any) {
+    const inputElement = event.target as HTMLInputElement;
+    const files = inputElement.files;
+    this.prepareFilesList(files);
+  }
+
+  /**
+   * Delete file from files list
+   * @param index (File index)
+   */
+  deleteFile(index: number) {
+    if (this.files[index].progress < 100) {
+      console.log("Upload in progress.");
+      return;
     }
-    console.log('Drag Leave');
+    this.files.splice(index, 1);
   }
 
-  fileBrowseHandler(files: any): void {
+  /**
+   * Convert Files list to normal array list
+   * @param files (Files List)
+   */
+  prepareFilesList(files: any) {
+    for (const item of files) {
+      item.progress = 0;
+      this.files.push(item);
+    }
+    this.fileDropEl.nativeElement.value = "";
+    this.uploadFilesSimulator(0);
+  }
 
+  /**
+   * Simulate the upload process
+   */
+  uploadFilesSimulator(index: number) {
+    setTimeout(() => {
+      if (index === this.files.length) {
+        return;
+      } else {
+        const progressInterval = setInterval(() => {
+          if (this.files[index].progress === 100) {
+            clearInterval(progressInterval);
+            this.uploadFilesSimulator(index + 1);
+          } else {
+            this.files[index].progress += 5;
+          }
+        }, 200);
+      }
+    }, 1000);
+  }
+
+  /**
+   * format bytes
+   * @param bytes (File size in bytes)
+   * @param decimals (Decimals point)
+   */
+  formatBytes(bytes: number, decimals = 2) {
+    if (bytes === 0) {
+      return "0 Bytes";
+    }
+    const k = 1024;
+    const dm = decimals <= 0 ? 0 : decimals;
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
   }
 
 }
--- clean-gallen/src/app/citizen-main-app/dashboard/dnd.directive.ts
+++ clean-gallen/src/app/citizen-main-app/dashboard/dnd.directive.ts
@@ -5,30 +5,31 @@ import {Directive, EventEmitter, HostBinding, HostListener, Output} from '@angul
 })
 export class DndDirective {
 
-  @HostBinding('class.fileover') fileOver: boolean;
+  @HostBinding('class.fileover') fileOver: boolean = false;
   @Output() fileDropped = new EventEmitter<any>();
 
   // Dragover listener
-  @HostListener('dragover', ['$event']) onDragOver(evt) {
+  @HostListener('dragover', ['$event']) onDragOver(evt: any) {
     evt.preventDefault();
     evt.stopPropagation();
     this.fileOver = true;
   }
 
   // Dragleave listener
-  @HostListener('dragleave', ['$event']) public onDragLeave(evt) {
+  @HostListener('dragleave', ['$event']) public onDragLeave(evt: any) {
     evt.preventDefault();
     evt.stopPropagation();
     this.fileOver = false;
   }
 
   // Drop listener
-  @HostListener('drop', ['$event']) public ondrop(evt) {
+  @HostListener('drop', ['$event']) public ondrop(evt: any) {
     evt.preventDefault();
     evt.stopPropagation();
     this.fileOver = false;
     let files = evt.dataTransfer.files;
     if (files.length > 0) {
+      console.log(`You dropped ${files.length}`);
       this.fileDropped.emit(files);
     }
   }
